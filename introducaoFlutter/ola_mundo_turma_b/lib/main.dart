815c30ce1d47d3f2090d39960a11d5a962ffd177
luizasuyoko <luizasuemiyokomizo@outlook.com>
2025-03-19 16:45:05 -0300

    19.03.2025

---
+++ introducaoFlutter/ola_mundo_turma_b/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());//clase principal da aplicação
+}
+
+//criar a classe principal
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Exemplo de Uso do Pubspec"),
+        ),
+        body: Center(
+          child: ElevatedButton(onPressed: ()=> ({
+            
+          }), child: Text("Clique Aqui!")),
+        ),
+      ),
+    );
+  }
+}
